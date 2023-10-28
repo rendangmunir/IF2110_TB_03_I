9c6f08ade43403701bd219f0a3b41f41c9ff43e7
rendangmunir <130053462+rendangmunir@users.noreply.github.com>
2023-10-28 13:28:16 +0700

    fix noHP

--- main/commands.c
+++ main/commands.c
@@ -397,7 +397,7 @@ void Ganti_Profil(){
                 printf("\n");
             }
         }
-        currentUser.noHP=WordToInt(currentWord);
+        currentUser.noHP= (currentWord);
         printf("\n");
 
         boolean validwt=false;
