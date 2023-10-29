434a1be935d8ec0351515733516ced7dc4f35824
Onta <mdz54dzakiarta@gmail.com>
2023-10-29 21:58:40 +0700

    Adding listutas

--- modules/listutas/listutas.c
+++ modules/listutas/listutas.c
@@ -27,13 +27,13 @@ void CreateUtas(EltypeUtas *utas, Word author, DATETIME datetime, Word text)
     (utas)->index = IDX_UNDEF;
     (utas)->author = author;
     (utas)->datetime = datetime;
-    (utas)->text = text; 
+    (utas)->text = text;
 }
 /****************** TEST LIST KOSONG ******************/
 boolean isEmpty(List l)
 /* Mengirim true jika list kosong */
 {
-    return NEXT(l) == NULL;
+    return l == NULL;
 }
 
 /****************** GETTER SETTER ******************/
@@ -41,10 +41,8 @@ EltypeUtas getUtas(List l, int idx)
 /* I.S. l terdefinisi, idx indeks yang valid dalam l, yaitu 0..length(l) */
 /* F.S. Mengembalikan nilai elemen l pada indeks idx */
 {
-    int i = 0;
     Address p = l;
-    while(i<idx){
-        i++;
+    while(INDEX(p)<idx){
         p = NEXT(p);
     }
     return INFO(p);
@@ -61,7 +59,7 @@ int indexOf(List l, EltypeUtas val)
 /* Mengembalikan IDX_UNDEF jika tidak ditemukan */
 {
     int i;
-    i = 0;
+    i = 1;
     Address p = l;
     boolean found = false;
     while(p != NULL && !found){
@@ -88,12 +86,19 @@ void insertFirst(List *l, EltypeUtas val)
 /* F.S. Melakukan alokasi sebuah elemen dan */
 /* menambahkan elemen pertama dengan nilai val jika alokasi berhasil. */
 {
-    Address p,loc;
-    p = newNodeUtas(val);
-    if (p != NULL){
-        loc = *l;
-        NEXT(p) = NEXT(loc);
-        NEXT(loc) = p;
+    Address newUtas,p,idx;
+    newUtas = newNodeUtas(val);
+    if (newUtas != NULL){
+        p = *l;
+        NEXT(newUtas) = *l;
+        *l = newUtas;
+        // Set idx
+        idx = 1;
+        while(p != NULL){
+            INDEX(p) = idx;
+            p = NEXT(p);
+            idx++;
+        }
     }
 }
 /* Jika alokasi gagal: I.S.= F.S. */
@@ -108,6 +113,7 @@ void insertLast(List *l, EltypeUtas val)
         insertFirst(l,val);
     }else{
         Address p = newNodeUtas(val);
+        INDEX(p) = length(*l);
         Address last = *l;
         if(p != NULL){
             while(NEXT(last) != NULL){
@@ -124,37 +130,47 @@ void insertAt(List *l, EltypeUtas val, int idx)
 /* menyisipkan elemen dalam list pada indeks ke-idx (bukan menimpa elemen di i) */
 /* yang bernilai val jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
 {
-    int cnt = 0;
-    Address p, loc;
-    if(idx==0){
+    Address p, loc,i;
+    if(idx==1){
         insertFirst(l, val);
     }
     else{
         p = newNodeUtas(val);
+        INDEX(p) = idx;
         if(p != NULL){
             loc = *l;
-            while(cnt<idx-1){
-                cnt++;
+            while(INDEX(loc) <idx-1){
                 loc = NEXT(loc);
             }
             NEXT(p) = NEXT(loc);
             NEXT(loc) = p;
+            loc = NEXT(loc);
+            i = idx;
+            while(loc != NULL){
+                INDEX(loc) = i;
+                i++;
+                loc = NEXT(loc);
+            }
         }
     }
 }
 
 /*** PENGHAPUSAN ELEMEN ***/
-void deleteFirst(List *l, EltypeUtas *val)
+void deleteFirst(List *l)
 /* I.S. List l tidak kosong  */
 /* F.S. Elemen pertama list dihapus: nilai info disimpan pada x */
 /*      dan alamat elemen pertama di-dealokasi */
 {
     Address p = *l;
-    p = NEXT(p);
     NEXT(*l) = NEXT(p);
     free(p);
+    p = NEXT(p);
+    while(p != NULL){
+        INDEX(p)--;
+        p = NEXT(p);
+    }
 }
-void deleteLast(List *l, EltypeUtas *val)
+void deleteLast(List *l)
 /* I.S. list tidak kosong */
 /* F.S. Elemen terakhir list dihapus: nilai info disimpan pada x */
 /*      dan alamat elemen terakhir di-dealokasi */
@@ -171,41 +187,66 @@ void deleteLast(List *l, EltypeUtas *val)
     else{
         NEXT(q) = NULL;
     }
-    *val = INFO(p);
     free(p);
 }
 
-void deleteAt(List *l, int idx, EltypeUtas *val)
+void deleteAt(List *l, int idx)
 /* I.S. list tidak kosong, idx indeks yang valid dalam l, yaitu 0..length(l) */
 /* F.S. val diset dengan elemen l pada indeks ke-idx. */
 /*      Elemen l pada indeks ke-idx dihapus dari l */
 {
-    if (idx == 0){
-        deleteFirst(l, val);
+    if (idx == 1){
+        deleteFirst(l);
     }else{
         Address p = *l;
-        Address q;
-        int i = 0;
-        while (i < idx-1){
+        Address temp;
+        while (INDEX(p) < idx-1){
             p = NEXT(p);
-            i++;
         }
-        q = NEXT(p);
-        *val = INFO(q);
-        NEXT(p) = NEXT(q);
-        free(q);
+        temp = NEXT(p);
+        NEXT(p) = NEXT(temp);
+        free(temp);
+        p = NEXT(p);
+        while(p != NULL){
+            INDEX(p)--;
+            p = NEXT(p);
+    }
     }
 }
 
 
 /****************** PROSES SEMUA ELEMEN LIST ******************/
-void displayList(List l);
+void displayUtas(List l)
 // void printInfo(List l);
 /* I.S. List mungkin kosong */
 /* F.S. Jika list tidak kosong, iai list dicetak ke kanan: [e1,e2,...,en] */
 /* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
 /* Jika list kosong : menulis [] */
 /* Tidak ada tambahan karakter apa pun di awal, akhir, atau di tengah */
+{
+    Address p = l;
+    while (p != NULL){
+        int index = INDEX(p);
+        Word author = AUTHOR(p);
+        DATETIME datetime = DATETIME(p);
+        Word text = TEXT(p);
+        
+        printf("\n");
+        printTab(2);
+        printf("ID = %d\n", index);
+        
+        printTab(2);
+        printWordNewline(author);
+
+        printTab(2);
+        TulisDATETIME(datetime);
+        printf("\n");
+
+        printTab(2);
+        printWordNewline(text);
+        p = NEXT(p);
+    }
+}
 
 int length(List l)
 /* Mengirimkan banyaknya elemen list; mengirimkan 0 jika list kosong */
@@ -224,7 +265,7 @@ int length(List l)
 }
 
 /****************** PROSES TERHADAP LIST ******************/
-List concat(List l1, List l2) ;
+List concat(List l1, List l2);
 /* I.S. l1 dan l2 sembarang */
 /* F.S. l1 dan l2 kosong, l3 adalah hasil konkatenasi l1 & l2 */
 /* Konkatenasi dua buah list : l1 dan l2    */
